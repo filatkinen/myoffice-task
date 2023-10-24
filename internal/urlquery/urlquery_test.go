d76175daac6cef87b6a45388aa9a18bb0b666217
filatkinen <filatkinen@gmail.com>
2023-10-24 22:28:11 +0300

    some fix

--- internal/urlquery/urlquery_test.go
+++ internal/urlquery/urlquery_test.go
@@ -1,40 +1,113 @@
 package urlquery
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"io"
 	"math/rand"
+	"net"
 	"net/http"
-	"net/http/httptest"
+	"os"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 )
 
-var src = rand.NewSource(time.Now().UnixNano())
-var r = rand.New(src)
+const maxSlice = 10000
+
+var (
+	host = "localhost"
+	port = "8089"
+)
 
 func TestUrlQuery(t *testing.T) {
 
-	httptest.NewServer()
-	hs := http.Server{Addr: "0.0.0.0:8089"}
-	hs.Handler = http.HandlerFunc(
-		func(writer http.ResponseWriter, request *http.Request) {
-			switch request.Method {
-			case http.MethodHead:
-				if randomBool() {
-					writer.Header().Set("Content-Length", "-1")
-				} else {
-					writer.Header().Set("Content-Length", "-1")
-				}
-				writer.Write([]byte(nil))
-			case http.MethodGet:
-
-			}
-		})
-	_ = hs.ListenAndServe()
+	t.Run("10 url, 4 threads,  output os.Stdout", func(t *testing.T) {
+		testUrlQuery(t, 10, 4, os.Stdout)
+	})
+	t.Run("10k url, 100 threads, output discarded", func(t *testing.T) {
+		testUrlQuery(t, 10*1000, 100, io.Discard)
+	})
+	t.Run("100k url, 10k threads, output discarded", func(t *testing.T) {
+		testUrlQuery(t, 100*1000, 10*1000, io.Discard)
+	})
 }
 
-func randomBool() bool {
-	if r.Intn(2) == 1 {
+func testUrlQuery(t *testing.T, maxURL int, maxThreads int, output io.Writer) {
+	var src = rand.NewSource(time.Now().UnixNano())
+	var rnd = rand.New(src)
+	b := make([]byte, maxSlice)
+
+	hs := http.Server{Addr: net.JoinHostPort(host, port)}
+	hs.Handler = http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			size := rnd.Intn(maxSlice)
+			switch r.Method {
+			case http.MethodHead:
+				if randomBool(rnd) {
+					w.Header().Set("Content-Length", strconv.Itoa(size))
+				}
+				// empty Content-Length header, so next time CLI will come using method GET
+				time.Sleep(time.Millisecond * time.Duration(rnd.Intn(30)))
+				w.WriteHeader(http.StatusOK)
+			case http.MethodGet:
+				time.Sleep(time.Millisecond * time.Duration(rnd.Intn(30)+30))
+				w.Write(b[:size])
+			}
+		})
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := hs.ListenAndServe()
+		require.Error(t, err, http.ErrServerClosed)
+	}()
+
+	// waiting till http server start
+	time.Sleep(time.Millisecond * 200)
+
+	urlGen := newURLGenerator("http://"+host+":"+port, maxURL)
+
+	urlQuery, err := New(&urlGen, output, maxThreads, "testing agent:))")
+	require.NoError(t, err)
+
+	urlQuery.Start()
+
+	err = hs.Close()
+	require.NoError(t, err)
+
+	wg.Wait()
+
+	fmt.Println("Results:")
+	fmt.Println(urlQuery)
+
+}
+
+func randomBool(rnd *rand.Rand) bool {
+	if rnd.Intn(2) == 1 {
 		return true
 	}
 	return false
+}
+
+type urlGenerator struct {
+	count   int
+	baseURL string
+	maxURL  int
+}
+
+func newURLGenerator(baseURL string, maxURL int) urlGenerator {
+	return urlGenerator{baseURL: baseURL, maxURL: maxURL}
+}
+
+func (u *urlGenerator) Read(p []byte) (int, error) {
+	if u.count+1 > u.maxURL {
+		return 0, io.EOF
+	}
+	u.count++
+	url := u.baseURL + "/" + strconv.Itoa(u.count) + "\n"
+	copy(p, url)
+	return len(url), nil
 }
